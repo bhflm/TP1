1fceddacfa32d0c05c97949e19526adb9475f791
German <germanbonin@gmail.com>
2018-05-21 17:11:38 -0300

    Add files via upload

--- tp1.c
+++ tp1.c
@@ -3,17 +3,21 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define FIN_LINEA '\0'
 
 char** split(const char* str, char sep){ //MODULARIZAR LOS 2 IF
   if (sep=='\0') return NULL;
   size_t i=0,j=0,k=0,cant=1; //Arranca en +1 que es el null
-  while(str[i]!='\0'){
+
+  while(str[i]!=FIN_LINEA){
     if(str[i]==sep) cant++;
     i++;
   }
   char** strv=malloc(sizeof(char*)*cant);
+
   i=0;
-  while(str[i]!='\0'){
+
+  while(str[i]!=FIN_LINEA){
     if(str[i]==sep){
       strv[k]=strndup(&str[j],(i-j));
       j=i+1;
@@ -31,18 +35,35 @@ char** split(const char* str, char sep){ //MODULARIZAR LOS 2 IF
 }
 
 char* join(char** strv, char sep){
-  size_t i=0,largo=0;
+  size_t i=0,j=0,k=0,len=0;
   while(strv[i]!=NULL){
-    largo+= strlen(strv[i]);
+    len+=strlen(strv[i])+1;
     i++;
   }
-  char* join = malloc(sizeof(char)*(largo+i));
-
-  for(int j=0;j<i;j++){
-  strcat(joint,strv[j]);
-  if(strv[j+1]!=NULL) strcat(joint,&sep);
+  char* str=malloc(sizeof(char)*(len));
+  i=0;
+  while(strv[i]!=NULL){
+    if(strv[i][j]==FIN_LINEA){
+      if(k+1==len) str[k]=FIN_LINEA;
+      else str[k]=sep;
+      j=0,i++,k++;
+    }
+    else{
+      str[k]=strv[i][j];
+      j++,k++;
+    }
   }
-  return join;
+  return str;
+
+}
+
+void free_strv(char* strv[]){
+  size_t i=0;
+  while(strv[i]!=NULL){
+    free(strv[i]);
+    i++;
+  }
+  free(strv);
 }
 
 int main(){
@@ -50,7 +71,8 @@ int main(){
   char sep = ',';
   char bsep = ';';
   char** strv=split(orig,sep);
-  char* joined = join(strv,bsep);
-  printf("salida: %s\n",joined);
+  char* a = join(strv,bsep);
+  printf("join: %s\n",a);
+  free(strv);
   return 0;
 }
